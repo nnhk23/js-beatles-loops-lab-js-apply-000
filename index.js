336f6f1cea2e077fd8620c90165685966868cc7e
Kim Nguyen <nnhk23@gmail.com>
2020-08-22 21:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+var arr = [];
+function theBeatlesPlay (musician, instrument) {
+  for (let i = 0; i < musician.length; i++) {
+    arr.push(`${musician[i]} plays ${instrument[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts (fact) {
+  let i = 0;
+  var array = [];
+  while (i < fact.length) {
+    array.push(`${fact[i]}!!!`)
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var list = [];
+  do {
+    list = "I love the Beatles!"
+    number++
+  } while (
+    15 - number >= 0
+    );
+    return list
+}
